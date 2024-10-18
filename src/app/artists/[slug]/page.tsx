04320807b760bc26ccbd9d23e7b582a8b2f3adb0
Patrick <kujo9or@gmail.com>
2024-10-18 11:34:35 +0200

    fixes stuff

--- src/app/artists/[slug]/page.tsx
+++ src/app/artists/[slug]/page.tsx
@@ -2,11 +2,10 @@ import { getStoryblokApi, StoryblokStory } from "@storyblok/react/rsc";
 import { draftMode } from "next/headers";
 
 export const generateStaticParams = async () => {
-    const { isEnabled } = draftMode();
     const client = getStoryblokApi();
     const response = await client.getStories({
-        content_type: "tour",
-        version: process.env.NODE_ENV === "development" || isEnabled
+        content_type: "artist",
+        version: process.env.NODE_ENV === "development"
             ? "draft"
             : "published",
     });
