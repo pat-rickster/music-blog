0d7ec89b8767005714b1d17a8ca9b83446800ff3
Patrick <kujo9or@gmail.com>
2024-10-18 09:20:10 +0200

    removes console log, removes unused variable

--- src/components/FeaturedArtist.tsx
+++ src/components/FeaturedArtist.tsx
@@ -1,8 +1,6 @@
 import Link from "next/link";
 
 export const FeaturedArtist = (props: any) => {
-    console.log(props.story.content.on_tour);
-    const onTour = props.story.content.on_tour;
     return (
         <div className="bg-white rounded-md shadow-md overflow-hidden relative">
             {props.story.content.on_tour ? <span className="bg-gray-100 text-gray-700 rounded-md py-1 px-2 uppercase font-bold text-xs absolute top-3 right-3">On Tour</span> : ""}
