4865344dfae6a76317b64004dae5e08ec0ec2050
Patrick <kujo9or@gmail.com>
2024-10-25 22:36:09 +0200

    adds fix

--- src/api/draft/route.ts
+++ src/api/draft/route.ts
@@ -5,6 +5,7 @@ import { NextRequest } from "next/server";
 export const GET = async (request: NextRequest) => {
     const { searchParams } = new URL(request.url);
     const slug = searchParams.get("slug");
-    (await draftMode()).enable();
+    const draft = await draftMode();
+    draft.enable();
     redirect(`/${slug}?${searchParams.toString()}`);
 };
